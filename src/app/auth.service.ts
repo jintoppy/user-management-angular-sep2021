6136ceb993b29e3af88fa85b9ca4c3d9b1a90487
Jinto Jose <jintoppy@gmail.com>
2021-09-28 11:27:49 +0530

    auth login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { of } from 'rxjs';
+import { tap, catchError} from 'rxjs/operators';
+
+type LoginResponse = {
+  token: string;
+  status: string;
+  msg?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +17,29 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  login(){
-
+  login(loginDetails: {username: string, password: string}){
+    return this.http
+              .post<LoginResponse>(`${environment.apiBaseUrl}/auth/login`, loginDetails)
+              .pipe(
+                tap((res) => {
+                  console.log(res);
+                  this.setToken(res.token);
+                }),
+                catchError(resp => {
+                  return of({
+                    token: '',
+                    status: 'failed',
+                    msg: resp.error.msg
+                  })
+                })
+              )
   }
 
-  setToken(){
-
+  setToken(token:string){
+    localStorage.setItem('token', token);
   }
 
   getToken(){
-    
+
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +9,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  errorMsg = '';
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(ngForm: NgForm){
-
+    console.log(ngForm.form.value);
+    if(ngForm.valid){
+      this.auth.login(ngForm.form.value)
+        .subscribe(res => {
+          if(res.msg){
+            this.errorMsg = res.msg;
+          }
+          else {
+            this.router.navigateByUrl('/');
+          }
+        })        
+    }
   }
 
 }
