b9d09a692b1222347d128185391d5f2604de8a30
Jinto Jose <jintoppy@gmail.com>
2021-09-28 09:55:02 +0530

    adding files

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    AddUserComponent,
+    UserDetailsComponent
   ],
   imports: [
     CommonModule,
