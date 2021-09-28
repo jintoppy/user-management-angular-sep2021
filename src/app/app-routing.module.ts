90f8a62bd3b3c183e99eee5605163729c91aaed4
Jinto Jose <jintoppy@gmail.com>
2021-09-28 09:42:31 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+    path: 'users', 
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule) 
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
