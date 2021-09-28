44849f1127fc374f5d9116364d2c321d1c2f46f5
Jinto Jose <jintoppy@gmail.com>
2021-09-28 11:29:06 +0530

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [{ 
     path: 'users', 
-    loadChildren: () => import('./users/users.module').then(m => m.UsersModule) 
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -40,6 +40,6 @@ export class AuthService {
   }
 
   getToken(){
-
+    return localStorage.getItem('token');
   }
 }
